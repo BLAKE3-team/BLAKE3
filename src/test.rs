1ed705374d89a373fad7aa5c1d66821267100f08
Jack O'Connor <oconnor663@gmail.com>
2019-12-04 16:55:50 -0500

    add a few tests for the arithmetic helpers

--- src/test.rs
+++ src/test.rs
@@ -1,4 +1,6 @@
+use crate::CHUNK_LEN;
 use core::cmp;
+use core::usize;
 
 pub const TEST_KEY: [u8; crate::KEY_LEN] = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
@@ -27,4 +29,53 @@ fn test_reference_impl_size() {
     // expected values. For now, the purpose of this test is to make sure we
     // notice if that happens.
     assert_eq!(1848, core::mem::size_of::<reference_impl::Hasher>());
+}
+
+#[test]
+fn test_offset_words() {
+    let offset: u64 = (1 << 32) + 2;
+    assert_eq!(crate::offset_low(offset), 2);
+    assert_eq!(crate::offset_high(offset), 1);
+}
+
+#[test]
+fn test_largest_power_of_two_leq() {
+    let input_output = &[
+        // The zero case is nonsensical, but it does work.
+        (0, 1),
+        (1, 1),
+        (2, 2),
+        (3, 2),
+        (4, 4),
+        (5, 4),
+        (6, 4),
+        (7, 4),
+        (8, 8),
+        // the largest possible usize
+        (usize::MAX, (usize::MAX >> 1) + 1),
+    ];
+    for &(input, output) in input_output {
+        assert_eq!(
+            output,
+            crate::largest_power_of_two_leq(input),
+            "wrong output for n={}",
+            input
+        );
+    }
+}
+
+#[test]
+fn test_left_len() {
+    let input_output = &[
+        (CHUNK_LEN + 1, CHUNK_LEN),
+        (2 * CHUNK_LEN - 1, CHUNK_LEN),
+        (2 * CHUNK_LEN, CHUNK_LEN),
+        (2 * CHUNK_LEN + 1, 2 * CHUNK_LEN),
+        (4 * CHUNK_LEN - 1, 2 * CHUNK_LEN),
+        (4 * CHUNK_LEN, 2 * CHUNK_LEN),
+        (4 * CHUNK_LEN + 1, 4 * CHUNK_LEN),
+    ];
+    for &(input, output) in input_output {
+        assert_eq!(crate::left_len(input), output);
+    }
 }
