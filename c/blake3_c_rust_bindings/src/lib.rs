28701d1585b5f0bd42a422f3e7c1a57fc878b975
Jack O'Connor <oconnor663@gmail.com>
2020-01-16 18:29:20 -0500

    add a README.md in c/blake3_c_rust_bindings

--- c/blake3_c_rust_bindings/src/lib.rs
+++ c/blake3_c_rust_bindings/src/lib.rs
@@ -1,7 +1,7 @@
 //! These are Rust bindings for the C implementation of BLAKE3. As there is a
-//! native Rust implementation of BLAKE3 provided in this same repo, these
-//! bindings are not expected to be used in production. They're intended for
-//! testing and benchmarking.
+//! native (and faster) Rust implementation of BLAKE3 provided in this same
+//! repo, these bindings are not expected to be used in production. They're
+//! intended for testing and benchmarking.
 
 use std::ffi::{c_void, CString};
 use std::mem::MaybeUninit;
