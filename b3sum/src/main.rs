989c30e2498c5ef65949761764297f9df864d074
Zach Crownover <zachary.crownover@gmail.com>
2022-03-26 01:48:20 +0000

    RUSTSEC-2020-0077
    Migrate from the abandoned memmap library to the now maintained fork
    of memmap2

--- b3sum/src/main.rs
+++ b3sum/src/main.rs
@@ -187,7 +187,7 @@ impl Args {
 }
 
 enum Input {
-    Mmap(io::Cursor<memmap::Mmap>),
+    Mmap(io::Cursor<memmap2::Mmap>),
     File(File),
     Stdin,
 }
@@ -276,7 +276,7 @@ fn copy_wide(mut reader: impl Read, hasher: &mut blake3::Hasher) -> io::Result<u
 // Mmap a file, if it looks like a good idea. Return None in cases where we
 // know mmap will fail, or if the file is short enough that mmapping isn't
 // worth it. However, if we do try to mmap and it fails, return the error.
-fn maybe_memmap_file(file: &File) -> Result<Option<memmap::Mmap>> {
+fn maybe_memmap_file(file: &File) -> Result<Option<memmap2::Mmap>> {
     let metadata = file.metadata()?;
     let file_size = metadata.len();
     Ok(if !metadata.is_file() {
@@ -297,9 +297,9 @@ fn maybe_memmap_file(file: &File) -> Result<Option<memmap::Mmap>> {
         // Explicitly set the length of the memory map, so that filesystem
         // changes can't race to violate the invariants we just checked.
         let map = unsafe {
-            memmap::MmapOptions::new()
+            memmap2::MmapOptions::new()
                 .len(file_size as usize)
-                .map(&file)?
+                .map(file)?
         };
         Some(map)
     })
