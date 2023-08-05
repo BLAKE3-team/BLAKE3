6eb4b8e657d0693e5da7dbc76c77b42c819454ab
Jack O'Connor <oconnor663@gmail.com>
2023-08-05 19:10:27 +0800

    blake3_guts_riscv64gcv_hash_chunks with full aligned chunks

--- rust/guts/src/riscv64gcv.rs
+++ rust/guts/src/riscv64gcv.rs
@@ -4,13 +4,21 @@
 //! requires quite a lot of effort, including building Clang from master and building QEMU from a
 //! custom branch. Please don't expect this code to be usable on real hardware for some time.
 
-use crate::{CVBytes, Implementation};
+use crate::{CVBytes, Implementation, CHUNK_LEN};
 
 // NOTE: Keep this in sync with the same constant in assembly.
 pub(crate) const MAX_SIMD_DEGREE: usize = 16;
 
 extern "C" {
     fn blake3_guts_riscv64gcv_degree() -> usize;
+    fn blake3_guts_riscv64gcv_hash_chunks(
+        input: *const u8,
+        input_len: usize,
+        key: *const CVBytes,
+        counter: u64,
+        flags: u32,
+        transposed_output: *mut u32,
+    );
     fn blake3_guts_riscv64gcv_hash_parents(
         transposed_input: *const u32,
         num_parents: usize,
@@ -20,11 +28,33 @@ extern "C" {
     );
 }
 
+unsafe extern "C" fn hash_chunks(
+    input: *const u8,
+    input_len: usize,
+    key: *const CVBytes,
+    counter: u64,
+    flags: u32,
+    transposed_output: *mut u32,
+) {
+    if input_len % CHUNK_LEN == 0 && input as usize % 4 == 0 {
+        blake3_guts_riscv64gcv_hash_chunks(
+            input,
+            input_len,
+            key,
+            counter,
+            flags,
+            transposed_output,
+        );
+    } else {
+        crate::portable::hash_chunks(input, input_len, key, counter, flags, transposed_output);
+    }
+}
+
 pub fn implementation() -> Implementation {
     Implementation::new(
         blake3_guts_riscv64gcv_degree,
         crate::portable::compress,
-        crate::portable::hash_chunks,
+        hash_chunks,
         blake3_guts_riscv64gcv_hash_parents,
         crate::portable::xof,
         crate::portable::xof_xor,
