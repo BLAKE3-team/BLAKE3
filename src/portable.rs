c971c8e7c1fa35ec8c4d668a34f6a2d9952bfc1d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2025-03-10 01:51:53 +0200

    Make `portable` module's `const fn` opt-in

--- src/portable.rs
+++ src/portable.rs
@@ -3,6 +3,7 @@ use crate::{
     OUT_LEN,
 };
 
+#[cfg(feature = "const")]
 #[inline(always)]
 const fn g(state: &mut [u32; 16], a: usize, b: usize, c: usize, d: usize, x: u32, y: u32) {
     state[a] = state[a].wrapping_add(state[b]).wrapping_add(x);
@@ -15,6 +16,20 @@ const fn g(state: &mut [u32; 16], a: usize, b: usize, c: usize, d: usize, x: u32
     state[b] = (state[b] ^ state[c]).rotate_right(7);
 }
 
+#[inline(always)]
+#[cfg(not(feature = "const"))]
+fn g(state: &mut [u32; 16], a: usize, b: usize, c: usize, d: usize, x: u32, y: u32) {
+    state[a] = state[a].wrapping_add(state[b]).wrapping_add(x);
+    state[d] = (state[d] ^ state[a]).rotate_right(16);
+    state[c] = state[c].wrapping_add(state[d]);
+    state[b] = (state[b] ^ state[c]).rotate_right(12);
+    state[a] = state[a].wrapping_add(state[b]).wrapping_add(y);
+    state[d] = (state[d] ^ state[a]).rotate_right(8);
+    state[c] = state[c].wrapping_add(state[d]);
+    state[b] = (state[b] ^ state[c]).rotate_right(7);
+}
+
+#[cfg(feature = "const")]
 #[inline(always)]
 const fn round(state: &mut [u32; 16], msg: &[u32; 16], round: usize) {
     // Select the message schedule based on the round.
@@ -33,6 +48,26 @@ const fn round(state: &mut [u32; 16], msg: &[u32; 16], round: usize) {
     g(state, 3, 4, 9, 14, msg[schedule[14]], msg[schedule[15]]);
 }
 
+#[cfg(not(feature = "const"))]
+#[inline(always)]
+fn round(state: &mut [u32; 16], msg: &[u32; 16], round: usize) {
+    // Select the message schedule based on the round.
+    let schedule = MSG_SCHEDULE[round];
+
+    // Mix the columns.
+    g(state, 0, 4, 8, 12, msg[schedule[0]], msg[schedule[1]]);
+    g(state, 1, 5, 9, 13, msg[schedule[2]], msg[schedule[3]]);
+    g(state, 2, 6, 10, 14, msg[schedule[4]], msg[schedule[5]]);
+    g(state, 3, 7, 11, 15, msg[schedule[6]], msg[schedule[7]]);
+
+    // Mix the diagonals.
+    g(state, 0, 5, 10, 15, msg[schedule[8]], msg[schedule[9]]);
+    g(state, 1, 6, 11, 12, msg[schedule[10]], msg[schedule[11]]);
+    g(state, 2, 7, 8, 13, msg[schedule[12]], msg[schedule[13]]);
+    g(state, 3, 4, 9, 14, msg[schedule[14]], msg[schedule[15]]);
+}
+
+#[cfg(feature = "const")]
 #[inline(always)]
 const fn compress_pre(
     cv: &CVWords,
@@ -73,6 +108,48 @@ const fn compress_pre(
     state
 }
 
+#[cfg(not(feature = "const"))]
+#[inline(always)]
+fn compress_pre(
+    cv: &CVWords,
+    block: &[u8; BLOCK_LEN],
+    block_len: u8,
+    counter: u64,
+    flags: u8,
+) -> [u32; 16] {
+    let block_words = crate::platform::words_from_le_bytes_64(block);
+
+    let mut state = [
+        cv[0],
+        cv[1],
+        cv[2],
+        cv[3],
+        cv[4],
+        cv[5],
+        cv[6],
+        cv[7],
+        IV[0],
+        IV[1],
+        IV[2],
+        IV[3],
+        counter_low(counter),
+        counter_high(counter),
+        block_len as u32,
+        flags as u32,
+    ];
+
+    round(&mut state, &block_words, 0);
+    round(&mut state, &block_words, 1);
+    round(&mut state, &block_words, 2);
+    round(&mut state, &block_words, 3);
+    round(&mut state, &block_words, 4);
+    round(&mut state, &block_words, 5);
+    round(&mut state, &block_words, 6);
+
+    state
+}
+
+#[cfg(feature = "const")]
 pub const fn compress_in_place(
     cv: &mut CVWords,
     block: &[u8; BLOCK_LEN],
@@ -92,6 +169,27 @@ pub const fn compress_in_place(
     cv[7] = state[7] ^ state[15];
 }
 
+#[cfg(not(feature = "const"))]
+pub fn compress_in_place(
+    cv: &mut CVWords,
+    block: &[u8; BLOCK_LEN],
+    block_len: u8,
+    counter: u64,
+    flags: u8,
+) {
+    let state = compress_pre(cv, block, block_len, counter, flags);
+
+    cv[0] = state[0] ^ state[8];
+    cv[1] = state[1] ^ state[9];
+    cv[2] = state[2] ^ state[10];
+    cv[3] = state[3] ^ state[11];
+    cv[4] = state[4] ^ state[12];
+    cv[5] = state[5] ^ state[13];
+    cv[6] = state[6] ^ state[14];
+    cv[7] = state[7] ^ state[15];
+}
+
+#[cfg(feature = "const")]
 pub const fn compress_xof(
     cv: &CVWords,
     block: &[u8; BLOCK_LEN],
@@ -119,6 +217,35 @@ pub const fn compress_xof(
     crate::platform::le_bytes_from_words_64(&state)
 }
 
+#[cfg(not(feature = "const"))]
+pub fn compress_xof(
+    cv: &CVWords,
+    block: &[u8; BLOCK_LEN],
+    block_len: u8,
+    counter: u64,
+    flags: u8,
+) -> [u8; 64] {
+    let mut state = compress_pre(cv, block, block_len, counter, flags);
+    state[0] ^= state[8];
+    state[1] ^= state[9];
+    state[2] ^= state[10];
+    state[3] ^= state[11];
+    state[4] ^= state[12];
+    state[5] ^= state[13];
+    state[6] ^= state[14];
+    state[7] ^= state[15];
+    state[8] ^= cv[0];
+    state[9] ^= cv[1];
+    state[10] ^= cv[2];
+    state[11] ^= cv[3];
+    state[12] ^= cv[4];
+    state[13] ^= cv[5];
+    state[14] ^= cv[6];
+    state[15] ^= cv[7];
+    crate::platform::le_bytes_from_words_64(&state)
+}
+
+#[cfg(feature = "const")]
 const fn hash1<const N: usize>(
     input: &[u8; N],
     key: &CVWords,
@@ -150,7 +277,70 @@ const fn hash1<const N: usize>(
     *out = crate::platform::le_bytes_from_words_32(&cv);
 }
 
+#[cfg(not(feature = "const"))]
+fn hash1<const N: usize>(
+    input: &[u8; N],
+    key: &CVWords,
+    counter: u64,
+    flags: u8,
+    flags_start: u8,
+    flags_end: u8,
+    out: &mut CVBytes,
+) {
+    debug_assert!(N % BLOCK_LEN == 0, "uneven blocks");
+    let mut cv = *key;
+    let mut block_flags = flags | flags_start;
+    let mut slice = input.as_slice();
+    while slice.len() >= BLOCK_LEN {
+        let block;
+        (block, slice) = slice.split_at(BLOCK_LEN);
+        if slice.is_empty() {
+            block_flags |= flags_end;
+        }
+        let block = {
+            let ptr = block.as_ptr() as *const [u8; BLOCK_LEN];
+            // SAFETY: Sliced off correct length above
+            unsafe { &*ptr }
+        };
+
+        compress_in_place(&mut cv, block, BLOCK_LEN as u8, counter, block_flags);
+        block_flags = flags;
+    }
+    *out = crate::platform::le_bytes_from_words_32(&cv);
+}
+
+#[cfg(feature = "const")]
 pub const fn hash_many<const N: usize>(
+    mut inputs: &[&[u8; N]],
+    key: &CVWords,
+    mut counter: u64,
+    increment_counter: IncrementCounter,
+    flags: u8,
+    flags_start: u8,
+    flags_end: u8,
+    mut out: &mut [u8],
+) {
+    debug_assert!(out.len() >= inputs.len() * OUT_LEN, "out too short");
+    while !inputs.is_empty() {
+        let input;
+        (input, inputs) = inputs.split_first().expect("Not empty; qed");
+        let o;
+        (o, out) = out.split_at_mut(OUT_LEN);
+        let o = {
+            let ptr = o.as_mut_ptr() as *mut [u8; OUT_LEN];
+            // SAFETY: Sliced off correct length above
+            unsafe { &mut *ptr }
+        };
+
+        hash1(input, key, counter, flags, flags_start, flags_end, o);
+        if increment_counter.yes() {
+            counter += 1;
+        }
+    }
+}
+
+#[cfg(not(feature = "const"))]
+pub fn hash_many<const N: usize>(
     mut inputs: &[&[u8; N]],
     key: &CVWords,
     mut counter: u64,
