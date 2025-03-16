a2516b78c2617d9ad289aeec91a02cb05d70cc17
Jack O'Connor <oconnor663@gmail.com>
2025-03-15 22:15:59 -0700

    close the fd in example_tbb.c

--- c/example_tbb.c
+++ c/example_tbb.c
@@ -5,11 +5,12 @@
 #include <string.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
   // For each filepath argument, memory map it and hash it.
   for (int i = 1; i < argc; i++) {
-    // Memory map the file.
+    // Open and memory map the file.
     int fd = open(argv[i], O_RDONLY);
     if (fd == -1) {
       fprintf(stderr, "open failed: %s\n", strerror(errno));
@@ -33,9 +34,13 @@ int main(int argc, char **argv) {
     // Hash the mapped file using multiple threads.
     blake3_hasher_update_tbb(&hasher, mapped, statbuf.st_size);
 
-    // Unmap the file.
+    // Unmap and close the file.
     if (munmap(mapped, statbuf.st_size) == -1) {
       fprintf(stderr, "munmap failed: %s\n", strerror(errno));
+      return 1;
+    }
+    if (close(fd) == -1) {
+      fprintf(stderr, "close failed: %s\n", strerror(errno));
       return 1;
     }
 
