c7ecbae50c6e438a65df5846db9482a9fda4291b
Jack O'Connor <oconnor663@gmail.com>
2025-03-29 15:25:11 -0700

    more comments on chaining values

--- src/hazmat.rs
+++ src/hazmat.rs
@@ -57,26 +57,26 @@
 //! let chunk1 = [b'b'; CHUNK_LEN];
 //! let chunk2 = [b'c'; 42]; // The final chunk can be short.
 //!
-//! // Hash all three chunks. Chunks or subtrees that don't begin at the start of the input use
-//! // `set_input_offset` to say where they begin.
-//! let chunk0_hash = Hasher::new()
+//! // Compute the non-root hashes ("chaining values") of all three chunks. Chunks or subtrees
+//! // that don't begin at the start of the input use `set_input_offset` to say where they begin.
+//! let chunk0_cv = Hasher::new()
 //!     // .set_input_offset(0) is the default.
 //!     .update(&chunk0)
 //!     .finalize_non_root();
-//! let chunk1_hash = Hasher::new()
+//! let chunk1_cv = Hasher::new()
 //!     .set_input_offset(CHUNK_LEN as u64)
 //!     .update(&chunk1)
 //!     .finalize_non_root();
-//! let chunk2_hash = Hasher::new()
+//! let chunk2_cv = Hasher::new()
 //!     .set_input_offset(2 * CHUNK_LEN as u64)
 //!     .update(&chunk2)
 //!     .finalize_non_root();
 //!
-//! // Join the first two chunks with a non-root parent node.
-//! let parent_hash = merge_subtrees_non_root(&chunk0_hash, &chunk1_hash, Mode::Hash);
+//! // Join the first two chunks with a non-root parent node and compute its chaining value.
+//! let parent_cv = merge_subtrees_non_root(&chunk0_cv, &chunk1_cv, Mode::Hash);
 //!
-//! // Join that parent node and the third chunk at the root of the tree.
-//! let root_hash = merge_subtrees_root(&parent_hash, &chunk2_hash, Mode::Hash);
+//! // Join that parent node and the third chunk with a root parent node and compute the hash.
+//! let root_hash = merge_subtrees_root(&parent_cv, &chunk2_cv, Mode::Hash);
 //!
 //! // Double check that we got the right answer.
 //! let mut combined_input = Vec::new();
@@ -89,8 +89,8 @@
 //!
 //! Hashing many chunks together is important for performance, because it allows the implementation
 //! to use SIMD parallelism internally. ([AVX-512](https://en.wikipedia.org/wiki/AVX-512) for
-//! example needs 16 chunks to really get going.) We can reproduce `parent_hash` by hashing
-//! `chunk0` and `chunk1` at the same time:
+//! example needs 16 chunks to really get going.) We can reproduce `parent_cv` by hashing `chunk0`
+//! and `chunk1` at the same time:
 //!
 //! ```
 //! # fn main() {
@@ -98,24 +98,24 @@
 //! # use blake3::hazmat::{Mode, HasherExt, merge_subtrees_non_root, merge_subtrees_root};
 //! # let chunk0 = [b'a'; CHUNK_LEN];
 //! # let chunk1 = [b'b'; CHUNK_LEN];
-//! # let chunk0_hash = Hasher::new().update(&chunk0).finalize_non_root();
-//! # let chunk1_hash = Hasher::new().set_input_offset(CHUNK_LEN as u64).update(&chunk1).finalize_non_root();
-//! # let parent_hash = merge_subtrees_non_root(&chunk0_hash, &chunk1_hash, Mode::Hash);
+//! # let chunk0_cv = Hasher::new().update(&chunk0).finalize_non_root();
+//! # let chunk1_cv = Hasher::new().set_input_offset(CHUNK_LEN as u64).update(&chunk1).finalize_non_root();
+//! # let parent_cv = merge_subtrees_non_root(&chunk0_cv, &chunk1_cv, Mode::Hash);
 //! # let mut combined_input = Vec::new();
 //! # combined_input.extend_from_slice(&chunk0);
 //! # combined_input.extend_from_slice(&chunk1);
-//! let left_subtree_hash = Hasher::new()
+//! let left_subtree_cv = Hasher::new()
 //!     // .set_input_offset(0) is the default.
 //!     .update(&combined_input[..2 * CHUNK_LEN])
 //!     .finalize_non_root();
-//! assert_eq!(left_subtree_hash, parent_hash);
+//! assert_eq!(left_subtree_cv, parent_cv);
 //!
 //! // Using multiple updates gives the same answer, though it's not as efficient.
 //! let mut subtree_hasher = Hasher::new();
 //! // Again, .set_input_offset(0) is the default.
 //! subtree_hasher.update(&chunk0);
 //! subtree_hasher.update(&chunk1);
-//! assert_eq!(left_subtree_hash, subtree_hasher.finalize_non_root());
+//! assert_eq!(left_subtree_cv, subtree_hasher.finalize_non_root());
 //! # }
 //! ```
 //!
@@ -239,7 +239,7 @@ impl HasherExt for Hasher {
     }
 }
 
-/// Compute the maximum length of a subtree in bytes, given its starting offset in bytes.
+/// The maximum length of a subtree in bytes, given its starting offset in bytes
 ///
 /// If you try to hash more than this many bytes as one subtree, you'll end up merging parent nodes
 /// that shouldn't be merged, and your output will be garbage. [`Hasher::update`] will currently
@@ -322,7 +322,7 @@ fn test_max_subtree_len() {
 }
 
 /// Given the length in bytes of either a complete input or a subtree input, return the number of
-/// bytes that belong to the left child subtree. The rest belong to the right child subtree.
+/// bytes that belong to its left child subtree. The rest belong to its right child subtree.
 ///
 /// Concretely, this function returns the largest power-of-two number of bytes that's strictly less
 /// than `input_len`. This leads to a tree where all left subtrees are "complete" and at least as
@@ -354,14 +354,14 @@ fn test_max_subtree_len() {
 /// // us exactly where to split the input. Any other split would either panic (if we're lucky) or
 /// // lead to an incorrect root hash.
 /// let left_len = left_subtree_len(input_len as u64) as usize;
-/// let left_subtree_hash = Hasher::new()
+/// let left_subtree_cv = Hasher::new()
 ///     .update(&input[..left_len])
 ///     .finalize_non_root();
-/// let right_subtree_hash = Hasher::new()
+/// let right_subtree_cv = Hasher::new()
 ///     .set_input_offset(left_len as u64)
 ///     .update(&input[left_len..])
 ///     .finalize_non_root();
-/// let root_hash = merge_subtrees_root(&left_subtree_hash, &right_subtree_hash, Mode::Hash);
+/// let root_hash = merge_subtrees_root(&left_subtree_cv, &right_subtree_cv, Mode::Hash);
 ///
 /// // Double check the answer.
 /// assert_eq!(root_hash, blake3::hash(&input));
@@ -421,6 +421,19 @@ impl<'a> Mode<'a> {
 }
 
 /// "Chaining value" is the academic term for a non-root or non-final hash.
+///
+/// Besides just sounding fancy, it turns out there are security reasons to be careful about the
+/// difference between (root/final) hashes and (non-root/non-final) chaining values. The BLAKE3
+/// "compression function" has an [internal `flags`
+/// parameter](https://github.com/BLAKE3-team/BLAKE3/blob/1.7.0/reference_impl/reference_impl.rs#L79)
+/// that it mixes with its other inputs, and we use different `flags` [for root
+/// hashes](https://github.com/BLAKE3-team/BLAKE3/blob/1.7.0/reference_impl/reference_impl.rs#L154).
+/// Without this, BLAKE3 would be vulnerable to [length extension
+/// attacks](https://en.wikipedia.org/wiki/Length_extension_attack).
+///
+/// For a formal treatment of this problem, and an even more important tree hashing problem
+/// involving internal collisions, see [Sufficient conditions for sound tree and sequential hashing
+/// modes](https://keccak.team/files/TreeHashing.pdf) by the Keccak team.
 pub type ChainingValue = [u8; OUT_LEN];
 
 fn merge_subtrees_inner(
@@ -437,8 +450,7 @@ fn merge_subtrees_inner(
     )
 }
 
-/// Compute a non-root chaining value, similar to
-/// [`Hasher::finalize_non_root`](HasherExt::finalize_non_root).
+/// Compute a non-root parent node chaining value from two child chaining values.
 ///
 /// See the [module level examples](index.html#examples), particularly the discussion of valid tree
 /// structures. The left and right child chaining values can come from either
@@ -453,12 +465,16 @@ pub fn merge_subtrees_non_root(
     merge_subtrees_inner(left_child, right_child, mode).chaining_value()
 }
 
-/// Compute a root hash, similar to [`Hasher::finalize`](crate::Hasher::finalize).
+/// Compute a root hash from two child chaining values.
 ///
 /// See the [module level examples](index.html#examples), particularly the discussion of valid tree
 /// structures. The left and right child chaining values can come from either
 /// [`Hasher::finalize_non_root`](HasherExt::finalize_non_root) or [`merge_subtrees_non_root`].
 /// "Chaining value" is the academic term for a non-root or non-final hash.
+///
+/// Note that inputs of [`CHUNK_LEN`] or less don't produce any parent nodes and can't be hashed
+/// using this function. In that case you must get the root hash from [`Hasher::finalize`] (or just
+/// [`blake3::hash`](crate::hash)).
 pub fn merge_subtrees_root(
     left_child: &ChainingValue,
     right_child: &ChainingValue,
@@ -467,13 +483,16 @@ pub fn merge_subtrees_root(
     merge_subtrees_inner(left_child, right_child, mode).root_hash()
 }
 
-/// Return a root [`OutputReader`](crate::OutputReader), similar to
-/// [`Hasher::finalize_xof`](crate::Hasher::finalize_xof).
+/// Build a root [`OutputReader`](crate::OutputReader) from two child chaining values.
 ///
 /// See also the [module level examples](index.html#examples), particularly the discussion of valid
 /// tree structures. The left and right child chaining values can come from either
 /// [`Hasher::finalize_non_root`](HasherExt::finalize_non_root) or [`merge_subtrees_non_root`].
 /// "Chaining value" is the academic term for a non-root or non-final hash.
+///
+/// Note that inputs of [`CHUNK_LEN`] or less don't produce any parent nodes and can't be hashed
+/// using this function. In that case you must get the `OutputReader` from
+/// [`Hasher::finalize_xof`].
 ///
 /// # Example
 ///
@@ -485,15 +504,16 @@ pub fn merge_subtrees_root(
 /// // the final chunk can be shorter than CHUNK_LEN.
 /// let chunk0 = &[42; CHUNK_LEN];
 /// let chunk1 = b"hello world";
-/// let chunk0_hash = Hasher::new()
+/// let chunk0_cv = Hasher::new()
 ///     .update(chunk0)
 ///     .finalize_non_root();
-/// let chunk1_hash = Hasher::new()
+/// let chunk1_cv = Hasher::new()
 ///     .set_input_offset(CHUNK_LEN as u64)
 ///     .update(chunk1)
 ///     .finalize_non_root();
+///
 /// // Obtain a blake3::OutputReader at the root and extract 1000 bytes.
-/// let mut output_reader = merge_subtrees_root_xof(&chunk0_hash, &chunk1_hash, Mode::Hash);
+/// let mut output_reader = merge_subtrees_root_xof(&chunk0_cv, &chunk1_cv, Mode::Hash);
 /// let mut output_bytes = [0; 1_000];
 /// output_reader.fill(&mut output_bytes);
 ///
