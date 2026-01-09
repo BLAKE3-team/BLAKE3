BLAKE3-team/BLAKE3

commit 34d293eb2aa75005406d8a7d78687896f714e89a

silence -Wunused-function on MacOS-arm64
the #endif was in the wrong place, causing the error
/Users/runner/work/php-src/php-src/ext/hash/blake3/upstream_blake3/c/blake3_dispatch.c:112:5: error: unused function 'get_cpu_features' [-Werror,-Wunused-function]

full compiler log https://github.com/php/php-src/actions/runs/7762643678/job/21173438425?pr=13194

c/blake3_dispatch.c
@@ -89,7 +89,6 @@ static void cpuidex(uint32_t out[4], uint32_t id, uint32_t sid) {
 #endif
 }
 
-#endif
 
 enum cpu_feature {
   SSE2 = 1 << 0,
@@ -164,6 +163,7 @@ static
 #endif
   }
 }
+#endif
 
 void blake3_compress_in_place(uint32_t cv[8],
                               const uint8_t block[BLAKE3_BLOCK_LEN],

commit b78a5c52ea0161ba6e16302b15ba939971174e2f

Fix building the test application on non-x86 machines
On anything other than x86, `make -f Makefile.testing` fails due to
undefined symbols:
```
/usr/bin/ld: /tmp/ccojePfa.o: in function `main':
main.c:(.text.startup+0x758): undefined reference to `get_cpu_features'
/usr/bin/ld: main.c:(.text.startup+0x75c): undefined reference to
`g_cpu_features'
/usr/bin/ld: main.c:(.text.startup+0x760): undefined reference to
`g_cpu_features'
/usr/bin/ld: main.c:(.text.startup+0xcfc): undefined reference to
`g_cpu_features'
/usr/bin/ld: main.c:(.text.startup+0xd00): undefined reference to
`g_cpu_features'
collect2: error: ld returned 1 exit status
make: *** [Makefile.testing:50: all] Error 1
```

The regression was introduced by commit
34d293eb2aa75005406d8a7d78687896f714e89a.

Here is how the two affected symbols, `g_cpu_features` and
`get_cpu_features()`, are used in the different configurations.

| `IS_X86` | `BLAKE3_TESTING` | Use of symbols      |
| :------: | :--------------: | ------------------- |
| X        |                  | internal            |
| X        | X                | internal & external |
|          |                  | N/A                 |
|          | X                | external            |

c/blake3_dispatch.c
@@ -89,6 +89,9 @@ static void cpuidex(uint32_t out[4], uint32_t id, uint32_t sid) {
 #endif
 }
 
+#endif
+
+#if defined(IS_X86) || defined(BLAKE3_TESTING)
 
 enum cpu_feature {
   SSE2 = 1 << 0,